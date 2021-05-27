73e9b81a6dc0c2ef0e657a8f58fbe3b0f6997e89
Кузьмин Артур <dayteyashik@gmail.com>
2021-05-27 18:26:53 +0300

    github workflow

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-# import os
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t^vxy9x+eyhwk8dreg2d0wv62()b8y&8!+!0(s1h3jv8+_okat'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -91,13 +91,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        # os.environ.get('DB_ENGINE'),
-        'NAME': 'postgres',  # os.environ.get('DB_NAME'),
-        'USER': 'postgres',  # os.environ.get('POSTGRES_USER'),
-        'PASSWORD': 'postgres',  # os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',  # os.environ.get('DB_HOST'),
-        'PORT': '5432',  # os.environ.get('DB_PORT'),
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
